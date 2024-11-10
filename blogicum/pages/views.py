4b92a3965710a173628ade57ffdd71fd2a229bed
MK <vlrtmsq@gmail.com>
2024-11-10 22:55:44 +0300

    настройка урлов

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def about(request):
+    return HttpResponse('Страничка about')
+
+
+def rules(request):
+    return HttpResponse('Страничка rules')
