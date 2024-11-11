2e2a7ca5ab87d482f4beec1798f15e7cec2b3d29
MK <vlrtmsq@gmail.com>
2024-11-11 21:24:47 +0300

    вроде все

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name="base_blog"),
+    path('', views.index, name="index"),
     path('posts/<int:id>/', views.post_detail, name="post_detail"),
     path('category/<slug:category_slug>/',
          views.category_posts,
-         name="category_detail")
+         name="category_posts")
 ]
